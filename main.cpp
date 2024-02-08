a5331d65b1e885edd1cde0e99f85e04f0836e6f9
colorchanchan <115062603+colorchanchan@users.noreply.github.com>
2024-02-09 00:13:54 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,70 @@
+﻿#include"Input.h"
+#include"WinApp.h"
+#include"DirectXCommon.h"
+//#include"Sprite.h"
+//#include"SpriteCommon.h"
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+    Input* input_ = nullptr;
+
+    WinApp* winApp_ = nullptr;
+
+    DirectXCommon* dxCommon_ = nullptr;
+
+
+
+#pragma region WindowsAPI初期化処理
+
+    winApp_ = new WinApp();
+    winApp_->Initialize();
+
+#pragma endregion
+
+    // DirectX初期化処理　ここから
+
+    dxCommon_ = new DirectXCommon();
+    dxCommon_->Initialize(winApp_);
+    // DirectX初期化処理　ここまで
+#pragma endregion
+
+    input_ = new Input();
+    input_->Initialize(winApp_);
+
+    /* SpriteCommon* spriteCommon = new SpriteCommon();
+     spriteCommon->Initialize();
+
+     Sprite* sprite = new Sprite();
+     sprite->Initialize();*/
+
+
+     // ゲームループ
+    while (true) {
+        if (winApp_->Update() == true)
+        {
+            break;
+        }
+
+        input_->Update();
+
+        dxCommon_->PreDraw();
+
+        dxCommon_->PosDraw();
+
+    }
+
+    /* delete sprite;
+
+     delete spriteCommon;*/
+
+    delete input_;
+
+    winApp_->Finalize();
+
+    delete winApp_;
+
+    delete dxCommon_;
+
+    return 0;
+}
